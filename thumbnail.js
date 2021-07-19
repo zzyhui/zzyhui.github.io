cd3b0f38135ba70b7d7290d667d245575f6813dc
zzyhui <37799244+zzyhui@users.noreply.github.com>
2021-07-19 13:20:10 +0800

    缩略图更改大小

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+    	targetImage.classList.remove("small");
+	});
 });
